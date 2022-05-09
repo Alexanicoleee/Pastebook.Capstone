7045a816917d0b05ea34d2a8c29518a46e398143
dayle.villanda <dayle.villanda@pointwest.com.ph>
2022-05-09 13:06:07 +0800

    add console project for testing

---
+++ PasteBook.WebApi/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using PasteBook.WebApi.Data;
+using System;
+using System.Linq;
+
+namespace Test
+{
+    class DB : PasteBookDb
+    {   
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConfigurationHelper.GetInstance().GetProperty<string>("ConnectionString"));
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DB dB = new DB();
+            var users = dB.Users.ToList();
+            foreach (var user in users)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(user));
+            }
+        }
+    }
+}
