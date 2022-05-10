70fe3cf5afbe0b64c22e7d7382d78c69aa80c538
Alexanicoleee <alexandranicolecajes@gmail.com>
2022-05-10 09:50:14 +0800

    Comment Controller

--- PasteBook.WebApi/PasteBook.WebApi/Controllers/CommentController.cs
+++ PasteBook.WebApi/PasteBook.WebApi/Controllers/CommentController.cs
@@ -1,6 +1,51 @@
-﻿namespace PasteBook.WebApi.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasteBook.WebApi.Data;
+using PasteBook.WebApi.Models;
+using System.Threading.Tasks;
+
+namespace PasteBook.WebApi.Controllers
 {
-    public class CommentController
+    [Route("comments")]
+    [ApiController]
+    public class CommentController : ControllerBase
     {
+        public IUnitOfWork UnitOfWork { get; private set; }
+        public CommentController(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetComments()
+        {
+            var comments = await UnitOfWork.CommentRepository.FindAll();
+            return Ok(comments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetComment(int id)
+        {
+            var post = await UnitOfWork.CommentRepository.FindByPrimaryKey(id);
+            if (post is object)
+            {
+                return Ok(post);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                var newComment = await UnitOfWork.CommentRepository.Insert(comment);
+                await UnitOfWork.CommitAsync();
+
+                return StatusCode(StatusCodes.Status201Created, newComment);
+            }
+
+            return BadRequest();
+        }
     }
 }
